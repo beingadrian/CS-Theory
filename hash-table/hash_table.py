1e795bfea2f5048e7530d1a7e1d281ab76b6aab4
Adrian Wisaksana <dev@beingadrian.com>
2016-02-16 00:31:12 -0800

    Implement Hash Table with Linked List (with tests)

---
+++ hash-table/hash_table.py
@@ -0,0 +1,90 @@
+# Hash table implementation
+
+import sys
+sys.path.insert(0, '../singly-linked-list')
+from singly_linked_list import LinkedList
+
+class HashTable:
+    """
+    An implementation for the hash table data structure
+    """
+
+    def __init__(self, bucket_count):
+        self.bucket_count = bucket_count
+        self.buckets = []
+        for i in range(0, bucket_count):
+            self.buckets.append(LinkedList())
+        self.item_count = 0
+
+    def __getitem__(self, key):
+        return self.get_value_for_key(key)
+
+    def __setitem__(self, key, value):
+        self.set_value_for_key(key, value)
+
+    def __delitem__(self, key):
+        self.remove_key(key)
+
+    def __contains__(self, key):
+        return self.contains(key)
+
+    def set_value_for_key(self, key, value):
+        def set_item(current_node):
+            current_node.item = (key, value)
+
+        if self._iterate_until_key_found(key, set_item) is None:
+            bucket_index = self._get_bucket_index_for_key(key)
+            linked_list = self.buckets[bucket_index]
+            linked_list.insert_at_tail((key, value))
+
+        self.item_count += 1
+
+    def get_value_for_key(self, key):
+        def return_value(current_node):
+            return current_node.item[1]
+
+        return self._iterate_until_key_found(key, return_value)
+
+    def remove_key(self, key):
+        bucket_index = self._get_bucket_index_for_key(key)
+        linked_list = self.buckets[bucket_index]
+        previous = None
+        current_node = linked_list.head
+        while current_node is not None:
+            if current_node.item[0] == key:
+                deleted_tuple = current_node.item
+                if previous is None:
+                    linked_list.delete_head()
+                else:
+                    previous.next = current_node.next
+                self.item_count -= 1
+                return deleted_tuple
+            previous = current_node
+            current_node = current_node.next
+        return None
+
+    def contains(self, key):
+        def return_true_if_key_found(current_node):
+            if current_node:
+                return True
+
+        result = self._iterate_until_key_found(key, return_true_if_key_found)
+        if result is not None:
+            return result
+        return False
+
+    # helper methods
+
+    def _iterate_until_key_found(self, key, function):
+        bucket_index = self._get_bucket_index_for_key(key)
+        linked_list = self.buckets[bucket_index]
+        current_node = linked_list.head
+        while current_node is not None:
+            if current_node.item[0] == key:
+                return function(current_node)
+            current_node = current_node.next
+        return None
+
+    def _get_bucket_index_for_key(self, key):
+        hashed_key = hash(key)
+        return hashed_key % self.bucket_count
---
+++ hash-table/test_hash_table.py
@@ -0,0 +1,55 @@
+# Hash table tests
+
+import unittest
+from hash_table import HashTable
+
+class TestHashTable(unittest.TestCase):
+
+    def setUp(self):
+        self.hash_table = HashTable(13)
+
+    def test_init(self):
+        bucket_size = len(self.hash_table.buckets)
+        self.assertEqual(13, bucket_size)
+        self.assertEqual(bucket_size, self.hash_table.bucket_count)
+        self.assertEqual(0, self.hash_table.item_count)
+
+    def test_set_value_for_key(self):
+        self.hash_table["key"] = "Bob"
+        self.assertEqual(1, self.hash_table.item_count)
+        self.assertEqual("Bob", self.hash_table["key"])
+        self.hash_table["key"] = "Max"
+        self.assertEqual("Max", self.hash_table["key"])
+
+    def test_get_value_for_key(self):
+        self.hash_table["Bob"] = "male"
+        self.hash_table["Bob"] = "alpha male"
+        self.hash_table["Alexa"] = "female"
+        self.assertNotEqual("male", self.hash_table["Bob"])
+        self.assertEqual("alpha male", self.hash_table["Bob"])
+
+    def test_remove_key(self):
+        self.hash_table["Bob"] = "male"
+        self.assertEqual("male", self.hash_table["Bob"])
+        self.assertEqual(1, self.hash_table.item_count)
+        del self.hash_table["Bob"]
+        self.assertEqual(0, self.hash_table.item_count)
+        self.assertIsNone(self.hash_table["Bob"])
+
+    def test_contains(self):
+        self.hash_table["Bob"] = "male"
+        self.hash_table["Alex"] = "male"
+        self.assertTrue("Bob" in self.hash_table)
+        self.assertTrue("Alex" in self.hash_table)
+        self.assertFalse("Chris" in self.hash_table)
+
+    # test helper methods
+
+    def test_get_bucket_index_for_key(self):
+        index = self.hash_table._get_bucket_index_for_key("key")
+        self.assertTrue(index < self.hash_table.bucket_count)
+        self.assertTrue(isinstance(index, int))
+
+
+if __name__ == '__main__':
+    unittest.main()
